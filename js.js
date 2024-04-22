38833f836b2b4895b5450618749cc9437b89bb89
Gabriel Lages <128821870+GLagess@users.noreply.github.com>
2024-04-22 19:26:55 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+document.getElementById("btnSilvio").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/SS.html";
+});
+
+document.getElementById("btnTeia").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/Ateia.html";
+});
+
+document.getElementById("btnDivertidamente").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/Divertidamente2.html";
+});
+
+document.getElementById("btnDuble").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/Duble.html";
+});
+
+document.getElementById("btnFuriosa").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/furiosa.html";
+});
+document.getElementById("btnKraven").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/Kraven.html";
+});
+document.getElementById("btnMacaco").addEventListener("click", function() {
+    
+    window.location.href = "/Páginas Sites/Macaco.html";
+});
+document.getElementById("btnAvaliação").addEventListener("click", function() {
+    
+    window.location.href = "/Página formulário/formulario.html";
+});
